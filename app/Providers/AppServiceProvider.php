6f5b3838f7d83d24e3218aac5e35552bc13417c9
hossein-hg <ghiasihossein2015@gmail.com>
2025-03-13 22:22:31 +0330

    login removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Auth::loginUsingId(10);
+        
 
         Paginator::useBootstrap();
         view()->composer('admin.layouts.header',function ($view){
